3d26dcb45e2c66284b6e68cf3efd3edab15c33c8
Alexandre Chagas Brites <acbrites@inf.ufsm.br>
2024-10-19 16:19:25 -0300

    Substitui WinForms por WPF

---
+++ App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Windows;
+
+namespace ELC1013_T1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            string[] srcs =
+            {
+                """
+                ifthen{Se p{o time joga bem}, q{ganha o campeonato}}.
+                ifthen{Se o time not{não p{joga bem}}, r{o técnico é culpado}}.
+                ifthen{Se q{o time ganha o campeonato}, s{os torcedores ficam contentes}}.
+                Os torcedores not{não s{estão contentes}}.
+                Logo, r{o técnico é culpado}.
+                """,
+                """
+                thenif{p{O participante vai ao paredão} se or{q{o lider o indica} ou r{os colegas o escolhem}}}.
+                ifthen{Se and{p{o participante vai ao paredão} e s{chora}}, então t{ele conquista o público}}.
+                ifthen{Se t{o participante conquista o público}, ele not{não u{é eliminado}}}.
+                and{q{O lider indicou um participante} e u{ele foi eliminado}}.
+                Logo, o participante not{não s{chorou}}.
+                """,
+                """
+                ifthen{Se or{p{o programa é bom} ou q{passa no horário nobre}}, r{o público o assiste}}.
+                ifthen{Se and{r{o público assiste} e s{gosta}}, então t{a audiência é alta}}.
+                ifthen{Se t{a audiência é alta}, u{a propaganda é cara}}.
+                O programa, and{q{passa no horário nobre}, mas not{u{a propaganda é barata}}}.
+                Logo, o público not{não s{gosta do programa}}.
+                """
+            };
+            string src = srcs[2];
+            Parser p = new(src);
+            p.Parse();
+            Debug.WriteLine($"Is {(DirectTable.IsValid(p.Atomics, p.Premises) ? "" : "not ")}valid.");
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ELC1013_T1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InputTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string src = inputTextBox.Text;// new TextRange(inputTextBox.Document.ContentStart, inputTextBox.Document.ContentEnd).Text;
+            Parser parser = new Parser(src);
+            parser.Parse();
+
+            errorsTextBox.Inlines.Clear();
+            foreach (string error in parser.Errors)
+            {
+                errorsTextBox.Inlines.Add(error);
+                errorsTextBox.Inlines.Add(Environment.NewLine);
+            }
+
+            atomicsTextBox.Inlines.Clear();
+            for (int i = 0; i < parser.Atomics.Count; i++)
+            {
+                atomicsTextBox.Inlines.Add($"{i + 1}. ");
+                atomicsTextBox.Inlines.Add(parser.Atomics[i]);
+                atomicsTextBox.Inlines.Add(Environment.NewLine);
+            }
+
+            premissesTextBox.Inlines.Clear();
+            for (int i = 0; i < parser.Premises.Count; i++)
+            {
+                premissesTextBox.Inlines.Add($"{i + 1}. ");
+                premissesTextBox.Inlines.Add(string.Join(string.Empty, parser.Premises[i].PrintProposition()));
+                premissesTextBox.Inlines.Add(Environment.NewLine);
+            }
+
+            Inferencer inferencer = new Inferencer();
+            foreach (PremiseNode node in parser.Premises)
+            {
+                inferencer.Infer(node.node);
+            }
+
+            inferredTextBox.Inlines.Clear();
+            for (int i = 0; i < inferencer.Propositions.Count; i++)
+            {
+                inferredTextBox.Inlines.Add($"{i + 1}. ");
+                inferredTextBox.Inlines.Add(string.Join(string.Empty, inferencer.Propositions[i].PrintProposition()));
+                inferredTextBox.Inlines.Add(" | ");
+                inferredTextBox.Inlines.Add(string.Join(string.Empty, inferencer.Propositions[i].PrintGuess()));
+                inferredTextBox.Inlines.Add(Environment.NewLine);
+            }
+        }
+    }
+}
